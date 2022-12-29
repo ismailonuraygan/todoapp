82e314828c716f49cf01478a6b70571bcb23593b
onur <ionuraygan@gmail.com>
2022-12-29 14:47:14 +0300

    get request and added components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import { useState, useEffect } from "react";
+import TodoList from "./components/TodoList/TodoList";
+import Form from "./components/Form/Form";
 
 function App() {
+  const [todos, setTodos] = useState([]);
+
+  async function getData() {
+    const response = await fetch(
+      "https://630f2d283792563418893c3d.mockapi.io/todos"
+    ).then((res) => res.json());
+    setTodos(response);
+  }
+  useEffect(() => {
+    getData();
+  }, []);
+  console.log(todos);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <header>TODO APP</header>
+      <Form />
+      {<TodoList todos={todos} />}
     </div>
   );
 }
